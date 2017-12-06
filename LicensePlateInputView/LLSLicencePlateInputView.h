34144582b670c3dd3eee43eb5323f60578fecc34
liulishuo <liulishuo2017@163.com>
2017-12-06 20:28:15 +0800

    添加注释

--- LicensePlateInputView/LLSLicencePlateInputView.h
+++ LicensePlateInputView/LLSLicencePlateInputView.h
@@ -25,14 +25,14 @@
 
 @property (nonatomic, strong, readonly) NSString *carNumStr;
 @property (nonatomic, weak) UIViewController<LLSLicencePlateInputViewDelegate> *delegate;
-@property (nonatomic, assign) CGFloat bottomLineOffsetY;
-@property (nonatomic, assign) CGFloat bottomLineGap;
-@property (nonatomic, assign) CGFloat bottomLineHeight;
-@property (nonatomic, strong) UIFont *textFont;
-@property (nonatomic, strong) UIColor *textColor;
-@property (nonatomic, strong) UIColor *generalLineColor;
-@property (nonatomic, strong) UIColor *indicatorLineColor;
+@property (nonatomic, assign) CGFloat bottomLineOffsetY;//底部边框线Y轴偏移量
+@property (nonatomic, assign) CGFloat bottomLineGap;//底部边框线边距
+@property (nonatomic, assign) CGFloat bottomLineHeight;//底部边框线高度
+@property (nonatomic, strong) UIFont *textFont;//文本字体
+@property (nonatomic, strong) UIColor *textColor;//文本颜色
+@property (nonatomic, strong) UIColor *generalLineColor;//底部边框线一般颜色
+@property (nonatomic, strong) UIColor *indicatorLineColor;//底部边框线指示颜色
 
-- (void)setCarNumber:(NSString *)carNumber;
+- (void)setCarNumber:(NSString *)carNumber;//填充文本
 
 @end
